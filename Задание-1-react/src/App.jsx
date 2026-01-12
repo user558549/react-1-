82ece85be42b07706d8960a100fa622adbd8be4e
Konstantin Tarkhanov <Kostya.tarhanov@mail.ru>
2026-01-12 20:34:57 +0500

    transform jsx

--- Задание-1-react/src/App.jsx
+++ Задание-1-react/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import React from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -7,38 +8,54 @@ function App() {
 	const [count, setCount] = useState(0);
 	const data = new Date().getFullYear();
 
-	return (
-		<>
-			{/* Декларативный */}
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			{/* Декларативный */}
-			<div className="card">
-				{/* Императивный */}
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				{/* Декларативный */}
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-			<p>{data}</p>
-		</>
+	return React.createElement(
+		React.Fragment,
+		null,
+		React.createElement(
+			"div",
+			null,
+			React.createElement(
+				"a",
+				{ href: "https://vite.dev", target: "_blank" },
+				React.createElement("img", {
+					src: viteLogo,
+					className: "logo",
+					alt: "Vite logo",
+				})
+			),
+			React.createElement(
+				"a",
+				{ href: "https://react.dev", target: "_blank" },
+				React.createElement("img", {
+					src: reactLogo,
+					className: "logo react",
+					alt: "React logo",
+				})
+			)
+		),
+		React.createElement("h1", null, "Vite + React"),
+		React.createElement(
+			"div",
+			{ className: "card" },
+			React.createElement(
+				"button",
+				{ onClick: () => setCount((count) => count + 1) },
+				`count is ${count}`
+			),
+			React.createElement(
+				"p",
+				null,
+				"Edit ",
+				React.createElement("code", null, "src/App.jsx"),
+				" and save to test HMR"
+			)
+		),
+		React.createElement(
+			"p",
+			{ className: "read-the-docs" },
+			"Click on the Vite and React logos to learn more"
+		),
+		React.createElement("p", null, data)
 	);
 }
 
